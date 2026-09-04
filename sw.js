04f5a5c90ac1f3aa901ff8c0b249714247fd356d
TKJP92 <takayakotani@yahoo.co.jp>
2026-09-04 20:17:50 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "af-stamp-v2";
+const STATIC = ["/","/index.html","/manifest.json","/icon-192.svg","/icon-512.svg"];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));
+  self.skipWaiting();
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",e=>{
+  if(e.request.url.includes("tile.openstreetmap.org")){
+    e.respondWith(caches.open("af-tiles").then(c=>
+      c.match(e.request).then(r=>r||(fetch(e.request).then(res=>{c.put(e.request,res.clone());return res;})))
+    ));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("/index.html"))));
+});
